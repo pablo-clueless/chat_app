4c091b367c5eac5106b7a7b032fbb08af26a2ff5
Samson Okunola <smsnmicheal@gmail.com>
2022-07-02 01:27:44 +0100

    chat app init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+const apiKey = import.meta.env.VITE_API_KEY
+
+const App = () => {
+  return (
+    <div>
+      <h1>Chat App</h1>
+    </div>
+  )
+}
+
+export default App
