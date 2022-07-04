be6d5a29571a288f28a781b73cb9d7a2fe3fde43
Samson Okunola <smsnmicheal@gmail.com>
2022-07-04 15:40:55 +0100

    search bar

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # COMMUNITY PRO CHAT
 
 Built with
-![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![getstream](https://img.shields.io/badge/getstream-%20-white?style=for-the-badge) and ![Twilio](https://img.shields.io/badge/twilio-%20white?style=for-the-badge&logo=twilio&logoColor=%f22f46)
+
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![getstream](https://img.shields.io/badge/getstream-%20-white?style=for-the-badge)![Twilio](https://img.shields.io/badge/twilio-%20white?style=for-the-badge&logo=twilio&logoColor=%f22f46&labelColor=black)
 
 The app is a custom team messaging app built for [Community Pro](https://github.com/CommunityPro).
 
--- src/components/ChannelSearch.jsx
+++ src/components/ChannelSearch.jsx
@@ -1,15 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import { useChatContext } from 'stream-chat-react'
 
+import { ResultsDropdown } from './'
 import { SearchIcon } from '../assets'
 
-const ChannelSearch = () => {
+const ChannelSearch = ({setToggleContainer}) => {
+  const { client, setActiveChannel } = useChatContext()
   const [query, setQuery] = useState('')
   const [loading, setLoading] = useState(false)
+  const [teamChannels, setTeamChannels] = useState([])
+  const [directChannels, setDirectChannels] = useState([])
+
+  useEffect(() => {
+    if(!query) {
+      setTeamChannels([])
+      setDirectChannels([])
+    }
+  },[query])
 
   const getChannels = async(text) => {
     try {
-      // TODO: fetch channels
+      const channelResponse = client.queryChannels({
+        type: 'team', name: { $autocomplete: text }, members: { $in: [client.userID] }
+      })
+      const userResponse = client.queryUsers({
+        id: { $ne: client.userID }, name: { $autocomplete: text }
+      })
+      
+      const [channels, { users }] = await Promise.all([channelResponse, userResponse])
+      if(channels.length) setTeamChannels(channels)
+      if(users.length) setDirectChannels(users)
     } catch (error) {
       setQuery('')
     }
@@ -23,6 +43,11 @@ const ChannelSearch = () => {
     getChannels(e.target.value)
   }
 
+  const setChannel = (channel) => {
+    setQuery('')
+    setActiveChannel(channel)
+  }
+
   return (
     <div className='channel-search__container'>
       <div className="channel-search__input__wrapper">
@@ -31,6 +56,9 @@ const ChannelSearch = () => {
         </div>
         <input className='channel-search__input__text' type='text' value={query} onChange={onSearch} placeholder='Search' />
       </div>
+      {query && (
+        <ResultsDropdown teamChannels={teamChannels} directChannels={directChannels} loading={loading} setChannel={setChannel} setQuery={setQuery} setToggleContainer={setToggleContainer} />
+      )}
     </div>
   )
 }
